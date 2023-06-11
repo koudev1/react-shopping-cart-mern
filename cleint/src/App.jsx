6f8f016cb12e8443da1a653d4ebb01f75c4d91c7
koudev1 <ahmadfarouhdev@gmail.com>
2023-06-11 12:42:19 +0300

    product-comp

--- cleint/src/App.jsx
+++ cleint/src/App.jsx
@@ -1,12 +1,26 @@
+import React, {useState} from "react";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import Product from "./components/Products/Product";
+
+
+import data from "./data.json";
+
 
 function App() {
+    const [products, setProducts] = useState(data);
+
+
     return (
         <>
             <div className="layout">
                 <Header />
-                <main>content</main>
+                <main>
+                    <div className="wrapper">
+                        <Product products={products} />
+                        <div className="filters"> Filter </div>
+                    </div>
+                </main>
                 <Footer />
             </div>
         </>
