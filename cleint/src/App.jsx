75cf927d5448079c33d88c455faaf5b9a859843a
koudev1 <ahmadfarouhdev@gmail.com>
2023-06-15 13:27:20 +0300

    filter comp

--- cleint/src/App.jsx
+++ cleint/src/App.jsx
@@ -1,16 +1,44 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Product from "./components/Products/Product";
 
-
 import data from "./data.json";
 import Filter from "./components/Filter/Filter";
 
-
 function App() {
     const [products, setProducts] = useState(data);
+    const [size, setSize] = useState("");
+    const [sort, setSort] = useState("");
 
+    const handFilterBySize = (e) => {
+        setSize(e.target.value);
+        if (e.target.value == "ALL") {
+            setProducts(data);
+        } else {
+            let productsClone = [...products];
+            let newProducts = productsClone.filter(
+                (p) => p.sizes.indexOf(e.target.value) != -1
+            );
+            setProducts(newProducts);
+        }
+    };
+
+    const handFilterBySort = (e) => {
+        let order = e.target.value;
+        setSort(order);
+        let productsClone = [...products];
+        let newProducts = productsClone.sort(function (a, b) {
+            if (order == "lowest") {
+                return a.price - b.price;
+            } else if (order == "highest") {
+                return b.price - a.price;
+            } else {    
+                return a.id < b.id ? 1 : -1;
+            }
+        });
+        setProducts(newProducts);
+    };
 
     return (
         <>
@@ -19,7 +47,12 @@ function App() {
                 <main>
                     <div className="wrapper">
                         <Product products={products} />
-                        <Filter />
+                        <Filter
+                            size={size}
+                            sort={sort}
+                            handFilterBySize={handFilterBySize}
+                            handFilterBySort={handFilterBySort}
+                        />
                     </div>
                 </main>
                 <Footer />
--- cleint/src/components/Filter/Filter.jsx
+++ cleint/src/components/Filter/Filter.jsx
@@ -4,12 +4,16 @@ import "../../css/Filter/Filter.css";
 
 const Filter = (props) => {
     return (
-        <div className="filter">
+        <div className="filters">
             <div className="filter-title">Filter</div>
             <div className="num-products">Number of products 4</div>
             <div className="filter-by-size">
                 <span>Filter</span>
-                <select name="" id="" className="filter-select">
+                <select
+                    className="filter-select"
+                    value={props.size}
+                    onChange={props.handFilterBySize}
+                >
                     <option value="ALL">ALL</option>
                     <option value="XS">XS</option>
                     <option value="S">S</option>
@@ -21,10 +25,14 @@ const Filter = (props) => {
             </div>
             <div className="filter-by-size">
                 <span>Order</span>
-                <select name="" id="" className="filter-select">
-                    <option value="Latest">Latest</option>
-                    <option value="Lower">Lower</option>
-                    <option value="Highest">Highest</option>
+                <select
+                    className="filter-select"
+                    value={props.sort}
+                    onChange={props.handFilterBySort}
+                >
+                    <option value="latest">Latest</option>
+                    <option value="lowest">Lowest</option>
+                    <option value="highest">Highest</option>
                 </select>
             </div>
         </div>
