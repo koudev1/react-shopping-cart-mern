923aaf30cabc5f42ec4e116d6df019e6ad276dca
koudev1 <ahmadfarouhdev@gmail.com>
2023-06-18 23:43:58 +0300

    product server

---
+++ server/routes/routes.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+const Product = require("../models/productMpdel");
+
+router.get("/api/products", async (req, res) => {
+    const products = await Product.find();
+    res.send(products);
+});
+
+router.post("/api/products", async (req, res) => {
+    const newProduct = new Product(req.body);
+    const saveP = await newProduct.save();
+    res.send(saveP);
+});
+
+router.delete("/api/products/:id", async (req, res) => {
+    const deleteProduct = await Product.findByIdAndDelete(req.params.id);
+    res.send(deleteProduct);
+});
+
+
+module.exports = router;
